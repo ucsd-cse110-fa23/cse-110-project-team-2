74b2ef8953eab95047dbf12742ea63f246ff6089
IVenzon <ijv2543@gmail.com>
2023-11-06 15:22:54 -0800

    mostly finished creation of recipe

--- src/RecordScreen.java
+++ src/RecordScreen.java
@@ -1,4 +1,10 @@
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.json.JSONException;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -20,6 +26,7 @@ public class RecordScreen extends BorderPane {
     private String recipeType;
     private Boolean isRecording = false;
     private Recorder recorder;
+    private Whisper testWhisper;
 
     RecordScreen(String type) {
         recipeType = type;
@@ -28,6 +35,7 @@ public class RecordScreen extends BorderPane {
         recordButton = new Button("Record");
         recordButton.setStyle("-fx-background-color: #43ED58");
         recorder = new Recorder();
+        testWhisper = new Whisper();
         this.setTop(header);
         this.setCenter(recordButton);
         this.setBottom(footer);
@@ -38,7 +46,7 @@ public class RecordScreen extends BorderPane {
         return recipeType;
     }
 
-    public void toggleRecord() {
+    public void toggleRecord() throws JSONException, IOException, URISyntaxException {
         isRecording = !isRecording;
         if(isRecording) {
             recordButton.setText("Recording...");
@@ -49,12 +57,39 @@ public class RecordScreen extends BorderPane {
             recordButton.setText("Record");
             recordButton.setStyle("-fx-background-color: #43ED58;");
             recorder.stopRecording();
+            Path recording = Paths.get("./recording.wav");
+            //moveToNextScreen(testWhisper.transcribe(recording.toFile()));
+            moveToNextScreen("I have chicken and waffles.", getRecipeType());
         }
     }
 
     public void addListeners() {
         recordButton.setOnAction(e -> {
-            toggleRecord();
+            try {
+                toggleRecord();
+            } catch (JSONException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            } catch (URISyntaxException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
         });
+    }
+
+    public void moveToNextScreen(String transcription, String mealType) {
+        Scene scene = getScene();
+        Window screen = scene.getWindow();
+        if (screen instanceof Stage) {
+            Stage current = (Stage) screen;
+            TranscriptionScreen screenTwo = new TranscriptionScreen(transcription, mealType);
+            current.setTitle("Ingredients");
+            current.setScene(new Scene(screenTwo, 500, 500));
+            current.setResizable(false);
+            current.show();
+        }
     }
 }
