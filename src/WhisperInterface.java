2dd62436371044de66e5d7abbb0fb39c4d9da61c
Gao <algao@ucsd.edu>
2023-11-06 18:02:24 -0800

    Fixed Gradle?

--- src/WhisperInterface.java
+++
@@ -1,8 +0,0 @@
-import org.json.JSONException;
-import java.io.*;
-import java.net.*;
-import org.json.*;
-
-public interface WhisperInterface {
-    public String transcribe(File recording) throws IOException, URISyntaxException, JSONException ;
-}
