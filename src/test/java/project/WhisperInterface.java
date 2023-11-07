f28ead8934763753f6a90d3c47bfb7522d15b4a8
Kevin Ly <k9ly@ucsd.edu>
2023-11-07 00:58:16 -0800

    added tests

---
+++ src/test/java/project/WhisperInterface.java
@@ -0,0 +1,10 @@
+package project;
+
+import org.json.JSONException;
+import java.io.*;
+import java.net.*;
+import org.json.*;
+
+public interface WhisperInterface {
+    public String transcribe(File recording) throws IOException, URISyntaxException, JSONException ;
+}
