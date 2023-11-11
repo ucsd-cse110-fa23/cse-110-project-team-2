2768cb5ec3ee1fca5d752e3ee344abcdefbe0d5d
Kevin Ly <k9ly@ucsd.edu>
2023-11-11 14:10:34 -0800

    Update TranscriptionScreen.java

--- src/TranscriptionScreen.java
+++ src/TranscriptionScreen.java
@@ -9,7 +9,6 @@ public class TranscriptionScreen extends Screen{
     public static String mealType;
     private ChatGPT gpt;
     private String recipe;
-    private String recipeTitle;
 
     TranscriptionScreen(String transcription, String type){
         ingredients = transcription;
@@ -22,7 +21,7 @@ public class TranscriptionScreen extends Screen{
 
     @Override
     protected Screen createNextScreen() {
-        return new RecipeScreen(recipe, recipeTitle);
+        return new RecipeScreen(recipe);
     }
 
     @Override
@@ -42,42 +41,38 @@ public class TranscriptionScreen extends Screen{
         public void handle(ActionEvent e) 
         {
             gpt = new ChatGPT();
-            try {
-                recipe = "Ingredients: \n - 2 boneless chicken breasts, cut into 1" cubes - 2 eggs
-                - 2 cloves garlic, minced
-                - 1 onion, thinly sliced
-                - 2 tablespoons olive oil
-                - Salt and pepper, to taste
-                - 1/4 teaspoon dried oregano
-                - 1/4 teaspoon dried thyme
-                
-                Instructions:
-                
-                1. Preheat oven to 350 degrees F.
-                
-                2. In a pan over medium heat, heat olive oil and add chicken cubes. Cook for 3-5 minutes or until chicken is lightly browned and cooked through.
-                
-                3. Transfer chicken to a baking dish.
-                
-                4. In the same pan, sauté garlic and onion for 3-5 minutes or until soft.
-                
-                5. Add garlic and onion to the baking dish.
-                
-                6. In a small bowl, beat the eggs and pour over chicken and vegetables.
-                
-                7. Sprinkle oregano, thyme, salt, and pepper over the top.
-                
-                8. Bake in preheated oven for 25-30 minutes or until the eggs are cooked through.
-                
-                9. Enjoy!"
-                recipeTitle = "Chicken Egg 'Surprise'";
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            } catch (InterruptedException e1) {
-                e1.printStackTrace();
-            } catch (URISyntaxException e1) {
-                e1.printStackTrace();
-            }
+            // try {
+                recipe = "Herb-Crusted Chicken and Egg Bake\r\n" + //
+                        "\r\n" + //
+                        "Ingredients: \r\n" + //
+                        "-2 boneless, skinless chicken breasts, cut into cubes\r\n" + //
+                        "-2 large eggs\r\n" + //
+                        "-2 tablespoons of fresh herbs (such as thyme, oregano, rosemary, or sage)\r\n" + //
+                        "-2 garlic cloves, minced\r\n" + //
+                        "-¼ teaspoon of black pepper\r\n" + //
+                        "-2 tablespoons of olive oil\r\n" + //
+                        "-3 tablespoons of breadcrumbs\r\n" + //
+                        "\r\n" + //
+                        "Instructions:\r\n" + //
+                        "\r\n" + //
+                        "1. Preheat the oven to 350 degrees Fahrenheit.\r\n" + //
+                        "\r\n" + //
+                        "2. In a medium bowl, mix together the fresh herbs, garlic, black pepper, breadcrumbs, and olive oil.\r\n" + //
+                        "\r\n" + //
+                        "3. Place the cubed chicken in a baking dish and top with the herb mixture.\r\n" + //
+                        "\r\n" + //
+                        "4. Crack the eggs over the top of the chicken and spread them out evenly.\r\n" + //
+                        "\r\n" + //
+                        "5. Bake for 25 minutes until the chicken is cooked through and the eggs have set.\r\n" + //
+                        "\r\n" + //
+                        "6. Serve hot and enjoy!";
+            // } catch (IOException e1) {
+            //     e1.printStackTrace();
+            // } catch (InterruptedException e1) {
+            //     e1.printStackTrace();
+            // } catch (URISyntaxException e1) {
+            //     e1.printStackTrace();
+            // }
             Screen nextScreen = createNextScreen();
             changeScreen(nextScreen);
         } 
