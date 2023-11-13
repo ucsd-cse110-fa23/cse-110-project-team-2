f2522676521b552cd1eea3387222225e98a9284a
IVenzon <ijv2543@gmail.com>
2023-11-12 19:48:47 -0800

    added gradle files

--- src/client/Model.java
+++ src/client/Model.java
@@ -7,6 +7,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import org.json.JSONException;
+
 import java.net.URI;
 import java.net.URISyntaxException;
  
@@ -24,6 +27,36 @@ public class Model {
     }
     public String transcribe(File recording) throws IOException, URISyntaxException, JSONException {
         return transcriber.transcribe(recording);
+    }
+
+    public String performRequest(String method, String language, String query) {
+        // Implement your HTTP request logic here and return the response
+
+        try {
+            String urlString = "http://localhost:8100/";
+            if (query != null) {
+                urlString += "?=" + query;
+            }
+            URL url = new URI(urlString).toURL();
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod(method);
+            conn.setDoOutput(true);
+
+            if (method.equals("POST") || method.equals("PUT")) {
+                OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
+                out.write(language);
+                out.flush();
+                out.close();
+            }
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String response = in.readLine();
+            in.close();
+            return response;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return "Error: " + ex.getMessage();
+        }
     }
 }
 
