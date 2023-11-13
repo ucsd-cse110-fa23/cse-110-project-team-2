d9f86d8aa81e6ccb8a16c947fc365175528cf65f
Aure361 <114559967+Aure361@users.noreply.github.com>
2023-11-12 18:53:47 -0800

    Updated Model

--- src/client/Model.java
+++ src/client/Model.java
@@ -1,6 +1,35 @@
 package client;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
+ 
+
+public class Model {
+    private ChatGPT recipeMaker = new ChatGPT();
+    private Whisper transcriber = new Whisper();
+    
+    public String generateRecipe(String ingredients, String mealType) throws IOException, InterruptedException, URISyntaxException{
+        return recipeMaker.generate(ingredients, mealType);
+    }
+
+    public String generateTitle(String ingredients, String mealType) throws IOException, InterruptedException, URISyntaxException{
+        return recipeMaker.generateTitle(ingredients, mealType);
+    }
+    public String transcribe(File recording) throws IOException, URISyntaxException, JSONException {
+        return transcriber.transcribe(recording);
+    }
+}
+
+/*package client;
+
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -38,4 +67,5 @@ public class Model {
             return "Error: " + ex.getMessage();
         }
     }
-}
+}*/
+
