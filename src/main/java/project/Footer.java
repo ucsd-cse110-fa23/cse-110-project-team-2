e7876b5344fc0043035bfd3cf5f815dafc82a683
lykevin2341 <k9ly@ucsd.edu>
2023-11-27 21:16:10 -0800

    Moved all files to proper locations

---
+++ src/main/java/project/Footer.java
@@ -0,0 +1,22 @@
+package project;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+
+public class Footer extends HBox {
+    
+    public Button leftButton;
+    public Button centerButton;
+    public Button rightButton;
+
+    Footer() {
+        leftButton = new Button();
+        centerButton = new Button();
+        rightButton = new Button();
+
+        this.getChildren().addAll(leftButton, centerButton, rightButton);
+        this.setAlignment(Pos.CENTER);
+
+    }
+
+}
