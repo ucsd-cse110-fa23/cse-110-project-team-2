f53c11c0a39f12c018c72b9a458ca3fd32aa8e50
algaoucsd <algao@ucsd.edu>
2023-12-02 16:47:16 -0800

    Moving accounts functionality into own object

---
+++ src/server/Accounts.java
@@ -0,0 +1,12 @@
+package server;
+
+import java.util.*;
+import java.io.File;
+import org.json.JSONObject;
+
+public class Accounts {
+    private HashMap<String,String> accounts;
+    // Accounts(File accountsJSON){
+    //     JSONObject json = new JSONOB
+    // }
+}
